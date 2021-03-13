d319934450b6490d3f24e56e9e23898bb5ed3b4a
vini0puh <80149018+vini0puh@users.noreply.github.com>
2021-03-13 11:32:53 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -3,7 +3,7 @@ import configparser
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, MessageHandler, Filters
 import urllib.request
-LINK = 'https://ulkabo.github.io/bot-10-00/data/'
+LINK = 'https://vini0puh.github.io/my_bot/'
 '''
 TOK = None
 config = configparser.ConfigParser()
@@ -186,7 +186,7 @@ def conditions(update, context):
 
     
 def main():
-    updater = Updater("1612051672:AAHr1oAA9dLdABLUJKgZ7mX0lgYj1cULjSg", use_context=True)
+    updater = Updater("1632229549:AAHi9orWKmwen2vZSWK_yFuzPVZxAurx95I", use_context=True)
 
     dp = updater.dispatcher
 
